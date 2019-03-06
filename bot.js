43df28df370c3f33dd655eddde4b56d04bb83e88
Ahmedpup <45870421+Ahmedpup@users.noreply.github.com>
2019-03-06 17:41:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -105,7 +105,7 @@ client.on('message',async message => {
 }
  
     if(args[1] && args[1] === 'create') {
-      //if(level[author.id].level < 10) return message.channel.send('**# يجب أن يكون لديك 10 مستويات لعمل كلان , لتجميع مستويات تفاعل بالشات وسيتم حساب النقاط**');
+      if(level[author.id].level < 10) return message.channel.send('**# يجب أن يكون لديك 10 مستويات لعمل كلان , لتجميع مستويات تفاعل بالشات وسيتم حساب النقاط**');
       if(system[author.id].clan !== 'None') return message.channel.send('**# يجب عليك ان تخرج من الكلان الذي أنت به حاليا**');
  
       let m = await message.channel.send('**# أكتب أسم الكلان الان**');
@@ -1757,7 +1757,7 @@ let embed = new Discord.RichEmbed()
                 
                                  .addField('شكرا لدخولك سيرفر', `${member.guild.name}`,true)
                                    
- .setFooter("LegendSystem")
+ .setFooter("! PuP")
     .setTimestamp()
 
   channel.sendEmbed(embed);
@@ -3539,75 +3539,7 @@ client.on('message', message => {
     }
 });
 
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
- client.on('message', message => {
+
  client.on('message', message => {
 var prefix = "P.";
 var cats = ["http://palestine-kitchen.ps/wp-content/uploads/2017/12/%D9%86%D9%83%D8%AA-%D8%AF%D8%A8%D8%A7%D9%86%D8%A9.png","http://www.i7lm.com/wp-content/uploads/2017/04/136769797816.jpg","https://4.bp.blogspot.com/-p62zmDIDXmI/WKzqNt9smaI/AAAAAAAAC4Q/sW_bSIB8OaQhwOYFeplc3uzz8PBN7l3YACEw/s1600/13602501135.jpg","https://www.universemagic.com/images/2016/03/7938-2-or-1457539273.jpg","https://1.bp.blogspot.com/-yFk-FzHSyE8/WR9fmPcsCUI/AAAAAAAAE6c/AmvjLadOiLY9GiCqMLHgA121bY2RS_dCwCLcB/s1600/%25D9%2586%25D9%2583%25D8%25AA%2B%25D9%2585%25D8%25B6%25D8%25AD%25D9%2583%25D8%25A9%2B1.jpg","https://l7zaat.com/wp-content/uploads/2018/02/423.jpg","https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg","http://www.shuuf.com/shof/uploads/2018/02/08/jpg/shof_97d686082bdb0a2.jpg"];
