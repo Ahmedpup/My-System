1bc9c3dd1235c7344396d7da61da849d827d94ef
Ahmedpup <45870421+Ahmedpup@users.noreply.github.com>
2019-06-24 13:42:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5048,7 +5048,7 @@ client.on('message', message => {
 
 
 let anti = JSON.parse(fs.readFileSync("./antigreff.json", "UTF8"));
-let config = JSON.parse(fs.readFileSync("./co1nfig.json", "UTF8"));
+let config = JSON.parse(fs.readFileSync("./config.json", "UTF8"));
 client.on("message", message => {
     if (!message.channel.guild) return;
     let user = anti[message.guild.id + message.author.id]
